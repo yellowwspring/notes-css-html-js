0c93edb7a7aa93a1548f241f102a852abbc53bb8
yellowwspring <yellowwspring@users.noreply.github.com>
2021-02-21 14:41:25 +0300

    Added a project FAQ. Status:DONE

---
+++ fifty-projects-css-js-html/frequently-asked-questions/script-FAQ.js
@@ -0,0 +1,7 @@
+let toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
